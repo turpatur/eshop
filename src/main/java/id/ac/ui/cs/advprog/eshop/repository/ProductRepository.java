886aebadea7ac784103b0f1835db8886210a1a50
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-02-13 12:17:07 +0700

    fix: html aligning and product repository delete method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -51,8 +51,8 @@ public class ProductRepository {
                 );
             }
         }
-    }
-
     public void delete(String productId) {
         productData.removeIf(product -> product.getProductId().equals(productId));
         }
+    }
+
