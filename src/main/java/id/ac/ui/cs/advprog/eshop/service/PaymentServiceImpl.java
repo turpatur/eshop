83c40f7b3ef16d139840046751fd4c197c3e073d
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-03-07 19:22:06 +0700

    [GREEN] implement payment service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,4 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Service;
@@ -10,23 +15,44 @@ import java.util.Map;
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    @Autowired
+    private PaymentRepository paymentRepository;
+
     @Override
     public Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData) {
-    return null;
+        if (paymentRepository.findById(paymentId) == null){
+            Payment payment = new Payment(paymentId, method, paymentData);
+            paymentRepository.save(payment, order);
+            return payment;
+        }
+        return null;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-    return null;
+        Order order = paymentRepository.getOrder(payment.getId());
+        // Use the payment object passed in instead of retrieving a new one
+        payment.setStatus(status);
+
+        if (status.equals(PaymentStatus.SUCCESS.getValue())) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else if (status.equals(PaymentStatus.REJECTED.getValue())) {
+            order.setStatus(OrderStatus.FAILED.getValue());
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        paymentRepository.update(payment, status);
+        return payment;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-    return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-    return null;
+        return paymentRepository.findAll();
     }
 }
