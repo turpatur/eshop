2cf35caa319d93a2d5e0a1f70cbf55df19702f1a
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-03-07 16:10:39 +0700

    [GREEN] implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
@@ -11,24 +13,44 @@ import java.util.Map;
 
 @Repository
 public class PaymentRepository {
+    private List<Payment> paymentData = new ArrayList<>();
+    private Map<String, Order> paymentOrder = new HashMap<>();
 
     public Payment save(Payment payment, Order order) {
-        return null;
+        paymentData.add(payment);
+        if (payment.getStatus().equals(PaymentStatus.SUCCESS.getValue())) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else {
+            order.setStatus(OrderStatus.FAILED.getValue());
+        }
+        paymentOrder.put(payment.getId(), order);
+        return payment;
     }
 
     public void update(Payment payment, String status) {
-        return;
+        payment.setStatus(status);
+        Order order = paymentOrder.get(payment.getId());
+        if (status.equals(PaymentStatus.SUCCESS.getValue())) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else {
+            order.setStatus(OrderStatus.FAILED.getValue());
+        }
     }
 
     public Payment findById(String paymentId) {
-        return null;
+        for (Payment payment : paymentData) {
+            if (payment.getId().equals(paymentId)) {
+                return payment;
+            }
+        }
+            return null;
     }
 
     public List<Payment> findAll() {
-        return null;
+        return new ArrayList<>(paymentData);
     }
 
     public Order getOrder(String paymentId) {
-        return null;
+        return paymentOrder.get(paymentId);
     }
 }
